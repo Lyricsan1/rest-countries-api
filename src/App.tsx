b35a01472e56d2df99c9452ab9840f191b25d233
Douglas <programing2002@hotmail.com>
2023-09-29 07:44:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routers } from "./Routes";
+import Navbar from "./layout/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routers />
+    </>
+  );
+}
+
+export default App;
---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="w-full h-20 shadow-md">Navbar</div>;
+};
+
+export default Navbar;
