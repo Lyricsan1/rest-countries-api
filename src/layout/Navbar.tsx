0e3f27ff407cced7a5079ba9131994eab274b72b
Douglas <programing2002@hotmail.com>
2023-09-29 07:52:34 -0300

    new deploy settings

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Navbar = () => {
   return <div className="w-full h-20 shadow-md">Navbar</div>;
 };
--- src/pages/HomeDetail.tsx
+++ src/pages/HomeDetail.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HomeDetail = () => {
   return <div>HomeDetail</div>;
 };
